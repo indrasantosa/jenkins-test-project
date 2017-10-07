cefd5db820f3f7d0e83ab93762c92ffd99e395a8
Indra Santosa <indrasantosa90@gmail.com>
2017-10-08 01:58:34 +0800

    Update prod build

--- test/test.js
+++ test/test.js
@@ -3,6 +3,11 @@ var app = require('../index.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('Hello World!', done);
+    request(app).get('/')
+      .expect('Hello World!')
+      .end(function(err, res) {
+        if (err) throw err
+        done()
+      });
   });
 });
